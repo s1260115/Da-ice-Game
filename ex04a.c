ea1d246a708d7eb2a5ada82c98427cbb3095447f
Yuki Shiga <yukishiga@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 22:13:17 +0900

    third version

--- ex04a.c
+++ ex04a.c
@@ -11,5 +11,10 @@ d=rand()%6+1;
 c=a+b+d;
   
 printf("Rolling the dice...\nDie 1: %d\nDie 2: %d\nDie 3: %d\nTotal value: %d\n",a,b,d,c);
+ 
+ if(c>7){
+  printf("%s won!\n", name);
+}
+else printf("%s lost!\n", name);
   return 0;
 }
