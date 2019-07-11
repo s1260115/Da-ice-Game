fed6aa94e1317db994099308bce652eeca54ce7b
Yuki Shiga <yukishiga@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 22:26:46 +0900

    third version

--- ex04b.c
+++ ex04b.c
@@ -3,11 +3,18 @@
 #include <time.h>
 int main () {
 int a,b,c,d;
+char name[20];
 srand(time(NULL));
 a=rand()%6+1;
 b=rand()%6+1;
 c=a+b;
   
 printf("Rolling the dice...\nDie 1: %d\nDie 2: %d\n\nTotal value: %d\n",a,b,c);
+ 
+ if(c>7){
+  printf("%s won!\n", name);
+}
+else printf("%s lost!\n", name);
+
  return 0;
 }
