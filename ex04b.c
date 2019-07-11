d1463bb9633cbf199f7ab63ac0740fd2afba91ad
Yuki Shiga <yukishiga@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 22:28:39 +0900

    last version

--- ex04b.c
+++ ex04b.c
@@ -7,9 +7,11 @@ char name[20];
 srand(time(NULL));
 a=rand()%6+1;
 b=rand()%6+1;
-c=a+b;
+d=rand()%6+1;
+c=a+b+d;
   
-printf("Rolling the dice...\nDie 1: %d\nDie 2: %d\n\nTotal value: %d\n",a,b,c);
+printf("Rolling the dice...\nDie 1: %d\nDie 2: %d\nDie 3: %d\nTotal value: %d\n",a,b,d,c);
+  
  
  if(c>7){
   printf("%s won!\n", name);
