1610c7b3dd78a76cea6d8248d5c8e9d3469ad5ee
Yuki Shiga <yukishiga@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 21:53:07 +0900

    First version

---
+++ ex04a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main () {
+int a,b,c,d;
+char name[20];
+srand(time(NULL));
+a=rand()%6+1;
+b=rand()%6+1;
+d=rand()%6+1;
+c=a+b+d;
+  
+printf("Rolling the dice...\nDie 1: %d\nDie 2: %d\nDie 3: %d\nTotal value: %d\n",a,b,d,c);
+  return 0;
+}
